1162232fc593ca3f242502ea1aeac1a46abf5f12
Aayushi0x9 <twishabansu7788@gmail.com>
2023-12-01 09:28:46 +0530

    01/01/2023

--- EXAM_PR-9/FL_even_odd.c
+++ EXAM_PR-9/FL_even_odd.c
@@ -1,29 +1,29 @@
 #include <stdio.h>
 
-int main() 
+int main()
 {
     FILE *evenFile, *oddFile;
-    
+
     int i;
 
     evenFile = fopen("even_file.txt", "w");
     oddFile = fopen("odd_file.txt", "w");
 
-    if (evenFile != NULL || oddFile != NULL) 
+    if (evenFile != NULL || oddFile != NULL)
     {
         printf("File created Successfully...!!");
         for (i = 50; i <= 70; i++)
         {
             if (i % 2 == 0)
             {
-            fprintf(evenFile, "%d ", i);
-            } 
+                fprintf(evenFile, "%d ", i);
+            }
             else
             {
-            fprintf(oddFile, "%d ", i);
+                fprintf(oddFile, "%d ", i);
             }
             fclose(evenFile);
-           fclose(oddFile);
+            fclose(oddFile);
         }
     }
     else
